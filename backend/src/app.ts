84f7ca530b9c39b95919d250df20cedd8d0cbe43
Virender Chauhan <bikuchauhan786+github@gmail.com>
2025-01-10 18:58:21 +0530

    cors origin updated

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -30,7 +30,7 @@ app.use(cookieParser())
 
 // Configure CORS
 app.use(cors({
-  origin: 'http://localhost:5173',
+  origin: 'hospital-food-management-system.vercel.app',
   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
   allowedHeaders: 'Content-Type,Authorization',
   credentials:true
